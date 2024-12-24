be5b4357ae59afa0b8c39570de8b98e7e802c2f8
MohammedHassan07 <mhassancs260@gmail.com>
2024-12-24 22:23:34 +0530

    REFACTOR: added the /api in every end point
    FEAT: Added cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()  
+const cors = require('cors')
 const express = require('express')
 const DB_connection = require('./Config/DB_connection')
 
@@ -7,6 +8,10 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+  origin: '*'
+}))
 
 const port = process.env.PORT 
 
@@ -21,7 +26,7 @@ const patient_route = require('./routes/patient.routes/patient.routes')
 const doctor_routes = require('./routes/doctor.routes/doctors.routes')
 const pharma_route = require('./routes/pharmacy/pharma.routes')
 
-app.use('/hospital', hospital_route)
-app.use('/patient', patient_route)
-app.use('/doctor', doctor_routes)
-app.use('/pharma', pharma_route)
+app.use('/api/hospital', hospital_route)
+app.use('/api/patient', patient_route)
+app.use('/api/doctor', doctor_routes)
+app.use('/api/pharma', pharma_route)

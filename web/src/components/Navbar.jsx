772917f219df2331c132138d91c4265fd5ac80e3
MohammedHassan07 <mhassancs260@gmail.com>
2025-01-03 23:26:45 +0530

    FIX: Import the useNavigate in Navbar

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { CategoryContext } from '../context/CategoryContext'
 import logout from '../assets/icons/logout.png'
 
 const Navbar = () => {
 
     const categoryState = useContext(CategoryContext)
+    const navigate = useNavigate()
 
     // logout
     function handleLogout() {

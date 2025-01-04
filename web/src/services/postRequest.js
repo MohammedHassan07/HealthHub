38cef976d176df4944f54f49a17776690452bc4f
MohammedHassan07 <mhassancs260@gmail.com>
2025-01-05 00:20:16 +0530

    FEAT: Connected the server to create the patient profile

--- web/src/services/postRequest.js
+++ web/src/services/postRequest.js
@@ -5,7 +5,18 @@ export default async function postRequest(endPoint, data) {
     try {
 
         // console.log('post request --> ', endPoint)
-        const response = await axios.post(endPoint, data)
+
+        const { token } = JSON.parse(localStorage.getItem('tokenData'))
+
+        const config = {
+            headers: {
+
+                token: token
+            }
+        }
+
+        // console.log(token)
+        const response = await axios.post(endPoint, data, config)
 
         // console.log(response)
         return { response, flag: true }
